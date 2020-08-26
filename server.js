b26fc5d26efb23f09f23a54932d1108758a8609e
darchik <darchik88@gmail.com>
2020-08-26 02:56:30 -0400

    commiting proxy server completed

--- server.js
+++ server.js
@@ -4,12 +4,13 @@ const port = 3011;
 
 
 app.use((req, res, next) => {
-  console.log('===',req.url);
   res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3011');
+  console.log('===',req.url);
   if (req.url === '/') {
     res.redirect(301, `${req.url}?product-detail=P001`);
   }
   next();
+  // res.end()
 });
 
 app.use(express.static('./client'));
