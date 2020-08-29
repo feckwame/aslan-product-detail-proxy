1a817b07fecd3bd79950f0664ccadba3acb4a266
darchik <darchik88@gmail.com>
2020-08-28 22:26:59 -0400

    commiting wokring proxy server rendering 3 components

--- server.js
+++ server.js
@@ -1,10 +1,11 @@
 const express = require('express');
+const cors = require('cors');
 const app = express();
 const port = 3011;
 
-
+app.use(cors());
 app.use((req, res, next) => {
-  res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3011');
+  // res.setHeader('Access-Control-Allow-Origin', 'http://localhost:3011');
   console.log('===',req.url);
   if (req.url === '/') {
     res.redirect(301, `${req.url}?productId=P001`);
